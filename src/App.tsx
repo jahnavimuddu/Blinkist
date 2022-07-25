4f27516f88a665e62f0fd69364e8ec4d10bbfee9
jahnavimuddu <jahnavi.muddu@zemosolabs.com>
2022-07-25 12:34:46 +0530

    Resolving code smells

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,6 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import BookDetailsPage from "./pages/BookDetails";
 import { makeStyles } from "@mui/styles";
 import LoginButton from './components/auth0/Login';
-import LogoutButton from './components/auth0/Logout';
 import {useAuth0} from '@auth0/auth0-react';
 import React from 'react'
 
